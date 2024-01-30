5e925e12a6cb281cdd76e7fe216d4406268312da
Arsen Bayan <a_bayan@kbtu.kz>
2024-01-30 23:58:06 +0600

    lab2

--- 4tuples.py
+++ 4tuples.py
@@ -10,4 +10,6 @@ print(len(fruits))
 fruits = ("apple", "banana", "cherry")
 print(fruits[-1])
 
-#
+#Exercise 4
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
